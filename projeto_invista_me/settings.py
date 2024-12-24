4059649cf0a865a62f467d6c3ef6b158d0f7d197
amaurigeraldes <amaurimgeraldes@gmail.com>
2024-12-23 23:03:09 -0300

    Nova Atualização do arquivo settings.py com novas configurações no DATABASES

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-59t-2@9gpw)yjbb6sm$gfnyssz16n_^a4gl+&2q@q7a%!y)*b='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
